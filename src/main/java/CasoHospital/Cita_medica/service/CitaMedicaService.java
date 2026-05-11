ad826fe1b420fd03c66020191e170df0ac9eed1a
ValeWifilafisufi <val.castroo@duocuc.cl>
2026-05-10 23:59:19 -0400

    Se termina el service

---
+++ src/main/java/CasoHospital/Cita_medica/service/CitaMedicaService.java
@@ -0,0 +1,79 @@
+package CasoHospital.Cita_medica.service;
+
+import CasoHospital.Cita_medica.dto.CitaMedicaResponseDto;
+import CasoHospital.Cita_medica.model.CitaMedica;
+import CasoHospital.Cita_medica.repository.CitaMedicaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+
+public class CitaMedicaService {
+
+    private final CitaMedicaRepository citaMedicaRepository;
+
+    private CitaMedicaResponseDto mapToDto(CitaMedica cita){
+        return new CitaMedicaResponseDto(
+                cita.getNumCita(),
+                cita.getFolioBono(),
+                cita.getNumRegistro(),
+                cita.getNumRun(),
+                cita.getFechaCita(),
+                cita.getHoraCita(),
+                cita.getEstadoCita(),
+                cita.getValorCita()
+        );
+    }
+
+    public List<CitaMedicaResponseDto> obtenerTodas(){
+        return citaMedicaRepository.findAll().stream()
+                .map(this::mapToDto).collect(Collectors.toList());
+    }
+
+    public Optional<CitaMedicaResponseDto> buscarPorNroCita(Long nro){
+        return citaMedicaRepository.findById(nro).map(this::mapToDto);
+    }
+
+    //guardar
+    public CitaMedicaResponseDto guardar(CitaMedica cita){
+        CitaMedica citanueva = citaMedicaRepository.save(cita);
+        return mapToDto(citanueva);
+    }
+
+    //Actualizar
+    public Optional<CitaMedica> actualizar(Long nro, CitaMedica cita){
+        return citaMedicaRepository.findById(nro).map(existente ->
+                {existente.setFechaCita(cita.getFechaCita());
+                existente.setHoraCita(cita.getHoraCita());
+                existente.setEstadoCita(cita.getEstadoCita());
+                existente.setValorCita(cita.getValorCita());
+
+                return citaMedicaRepository.save(existente);
+                });
+     }
+
+    //Eliminar
+    public void eliminar(Long nro){citaMedicaRepository.deleteById(nro);}
+
+    //Busqueda personalizada
+    public List<CitaMedicaResponseDto> buscarPorFolio(Long folio){
+        return citaMedicaRepository.finByFolioBono(folio);
+    }
+
+    public List<CitaMedicaResponseDto> buscarPorNumRegistro(Long nro){
+        return citaMedicaRepository.finByNumRegistro(nro);
+    }
+
+    public List<CitaMedicaResponseDto> buscarNumRum(Long run){
+        return citaMedicaRepository.finByNumRun(run);
+    }
+
+    public List<CitaMedicaResponseDto> buscarPorCita(LocalTime fecha){
+        return citaMedicaRepository.findByFechaCita(fecha);
+    }
+}
